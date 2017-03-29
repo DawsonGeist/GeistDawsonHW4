98c2d5f99de431aa914880f4e08acff7f91490af
DawsonGeist <Dawson.geist@gmail.com>
2017-03-29 18:27:48 -0400

    Add files via upload

--- main.java
+++ main.java
@@ -5,21 +5,24 @@ public class main {
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		String one = "One";
-		String two = "Two";
-		String three = "Three";
+		Node one = new Node(1,"One");
+		Node two = new Node(2,"Two");
+		Node three = new Node(3, "Three");
 		Stack st = new Stack();
 		Queue qu = new Queue();
 		
+		System.out.println("testing of the queues\n\n");
 		qu.enqueue(one);
 		qu.enqueue(two);
 		qu.enqueue(three);
 		qu.dequeue();
 		System.out.println(qu.peak());
-		
+		System.out.println("\n\ntesting of the stacks\n\n");
 		st.push(one);
 		st.push(two);
-		System.out.println(st.pop());
+		st.push(three);
+		st.pop();
+		st.peak();
 		
 	}
 
